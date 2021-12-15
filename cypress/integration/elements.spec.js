208a0daadc88d33ac9731dfb6ece2643f0327b44
Júlio Martins <julio.neto@mt.estudante.senai.br>
2021-12-15 09:13:53 -0400

    links

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -13,4 +13,21 @@ describe('Work with basic elements', () => {
         //ou
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
     })
+
+    it('Links', () => {
+        //sempre colocar o visit nos testes individualmente
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        //acao de clicar no link
+        cy.get('[href="#"]').click()
+        //capturando o resultado do click
+        cy.get('#resultado').should('have.text', 'Voltou!')
+
+        //recarregando a pagina
+        cy.reload()
+        //Verificar que nao tem o texto voltou
+        cy.get('#resultado').should('have.not.text', 'Voltou!')
+        //usando contains para buscar atravez do texto presente na pagina HTML
+        cy.contains('Voltar').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+    })
 })
